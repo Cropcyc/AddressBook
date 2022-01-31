fabd3d1eb6b069b9d3cfe392aac42fcda9106f8b
Cropcyc <48459764+Cropcyc@users.noreply.github.com>
2022-01-31 11:06:58 +0100

    Docket Bean added to main class

--- src/main/java/com/jide/addressbook/AddressbookApplication.java
+++ src/main/java/com/jide/addressbook/AddressbookApplication.java
@@ -3,12 +3,20 @@ package com.jide.addressbook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.context.annotations.Bean;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 @EnableSwagger2
 public class AddressbookApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AddressbookApplication.class, args);
+	}
+
+	@Bean
+	public Docket apiInfor(){
+		return new Docket(DocumentationType.SWAGGER2).select()
+			.apis(RequestHandlerSelectors.basePackage("com.jide.addressbook")).build();
 	}
 
 }
